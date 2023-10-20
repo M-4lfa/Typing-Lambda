f9ab8085680dfbfc8be2fdbe7e356c832c557ce9
M_alfa <139083690+M-4lfa@users.noreply.github.com>
2023-10-19 19:44:34 -0600

    hice parser y gramatica en lark que no quiero borrar

--- Typinglexer/parser.py
+++ Typinglexer/parser.py
@@ -9,9 +9,9 @@ from typing import Union, Optional
 from dataclasses import dataclass
 import Typinglexer.lexer as LexerT
 
-Expression_Literal = Union["LexerT.Boole","LexerT.Int"]
-Expression_Function = Union["LexerT.Boole","LexerT.Int"]
-Expression_Type = Union["LexerT.Boole","LexerT.Int", "TypeExpression"]
+Expression_Literal = Union["LexerT.BoolExpresion","LexerT.Int"]
+Expression_Function = Union["LexerT.BoolExpresion","LexerT.Int"]
+Expression_Type = Union["LexerT.BoolExpresion","LexerT.Int", "TypeExpression"]
 
 Expression = Union[
     "Variable",
@@ -60,21 +60,53 @@ class TypeExpression:
 
 
 
-def parser_literal (tokens = Optional[list["LexerT.Token"]])->Optional[Literal]:
-    print(tokens,"newtokens")
-    if tokens is not None:
-        new_tokens = tokens
-        if isinstance(new_tokens[0], LexerT.BoolExpresion):
-            if new_tokens[0] == LexerT.BoolExpresion(True):
-                return Literal(LexerT.Bool(True))
-            else:
-                return Literal(LexerT.Bool(False))
-        elif isinstance(new_tokens[0], LexerT.Int):
-            return Literal(new_tokens[0])
+def parser_literal (tokens = list["LexerT.Token"])->Optional[(list["LexerT.Token"])]:
+    print(tokens,"newtokens en parsr.py")
+    if len(tokens)> 0:
+        if isinstance(tokens[0], LexerT.BoolExpresion) or isinstance(tokens[0], LexerT.Int):
+            Lit = Literal(tokens[0])
+            new_tokens = tokens[1:]
+            return new_tokens
         else:
             return None
     else:
         return None
+    
+def parser_aplication_expression (tokens = list["LexerT.Token"])->Optional[AplicationExpression]:
+    print(tokens,"newtokens")
+    if len(tokens)> 0:
+        if tokens[0] is LexerT.LeftP():
+            new_tokens = tokens[1:]
+            b = parser_expression(new_tokens)
+            if  b is not None: 
+                new_tokens, left_expression = parser_expression(new_tokens)
+            b = parser_expression(new_tokens)
+            if  b is not None:
+                new_tokens, right_expression = parser_expression(new_tokens)
+            return new_tokens, AplicationExpression(left_expression, right_expression), 
+        else:
+            return None
+        
+def parser_if_expression (tokens = list["LexerT.Token"])->Optional[IfExpression]:
+    print(tokens,"newtokens")
+    if len(tokens)> 0:
+        if tokens[0] is LexerT.If():
+            new_tokens = tokens[1:]
+            new_tokens, first_expression = parser_expression(new_tokens)
+            if tokens[0] is LexerT.Then():
+                new_tokens, second_expression = parser_expression(new_tokens)
+                if tokens[0] is LexerT.Else():
+                    new_tokens, second_expression = parser_expression(new_tokens)
+        else:
+            return tokens, None
+            
+def parser_expression (token_list = list["Lexer.Token"]):    
+    parser_list = [
+        parser_aplication_expression,
+        parser_literal  
+    ]
+    pass            
+        
     
     
 def main(): 
